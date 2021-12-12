4188db3b3be759a78fa0d731ec4af65acd871f38
Quadath <Quadatz@users.noreply.github.com>
2021-12-12 18:17:13 +0200

    object actions

--- js/script.js
+++ js/script.js
@@ -1,19 +1,32 @@
-const box = document.getElementById('box');
+const box = document.getElementById('box'),
+    btns = document.getElementsByTagName('button'),
+    circles = document.getElementsByClassName('circle'),
+    hearts = document.querySelectorAll(".heart"),
+    oneHeart = document.querySelector('.heart'),
+    wrapper = document.querySelector('.wrapper');
+box.style.backgroundColor = 'blue';
+box.style.width = '500px';
 
-console.log(box);
-
-const btns = document.getElementsByTagName('button')[1]; //Get second button
-console.log(btns);
-
-const circles = document.getElementsByClassName('circle');
-console.log(circles);
-
-const hearts = document.querySelectorAll(".heart");
-// console.log(hearts);
-
+btns[1].style.borderRadius = '100%';
+// circles.style.backgroundColor = 'red'; //WRONG
+for(let i = 0; i < circles.length; i++) {
+    hearts[i].style.backgroundColor = 'orange';
+}
 hearts.forEach(item => {
-    console.log(item);
+    item.style.backgroundColor = 'blue';
 });
+let num;
+box.style.cssText = `background-color: blue; width: ${num}px;`;
 
-const oneHeart = document.querySelector('.heart');
-console.log(oneHeart);
+const div = document.createElement('div');
+// const text = document.createTextNode('Gay sex');
+
+div.classList.add('black');
+// document.body.append(div);
+// wrapper.append(div);
+// wrapper.prepend(div)
+// hearts[0].before(div);
+// hearts[0].after(div);
+// circles[0].remove;
+// hearts[0].replaceWith(circles[0]);
+wrapper.replaceChildren(circles[0], hearts[0]);
