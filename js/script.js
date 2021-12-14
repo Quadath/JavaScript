b36cd5d2f95c8b8da19fe7fdcb3191728a383a57
Quadath <Quadatz@users.noreply.github.com>
2021-12-14 18:22:23 +0200

    amazing button

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 "use strict"
 
+const btn = document.querySelector('button');
+
+// btn.onclick = function() {
+//     alert('fuck you');
+// };
+
+// btn.addEventListener('click', () => 
+// {
+//     alert('Da');
+// });
+
+const deleteElement = (e) => {
+    e.target.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+};
+
+btn.addEventListener('mouseenter', deleteElement);
