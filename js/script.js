667586d197f508cfd007d07826b08f7abac93c1e
Quadath <Quadatz@users.noreply.github.com>
2021-12-18 07:04:25 +0200

    touches

--- js/script.js
+++ js/script.js
@@ -1,18 +1,12 @@
-"use strict"
+"use strict";
 
-// console.log(document.body.childNodes);
-// console.log(document.body.firstChild);
-// console.log(document.body.lastChild);
-
-// console.log(document.querySelector('#current').parentNode);
-// console.log(document.querySelector('#current').parentElement);
-
-// console.log(document.querySelector('[data-current="3"]').nextSibling);
-// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
-
-for(let node of document.body.childNodes) {
-    if(node.nodeName == '#text') {
-        continue;
-    }
-    console.log(node);
-}
+window.addEventListener('DOMContentLoaded', () => {
+    const box = document.querySelector('.box');
+    box.addEventListener('touchmove', (e) => {
+        e.preventDefault();
+        // console.log(' ');
+        // console.log(e.touches);
+        // console.log(e.targetTouches);
+        console.log(e.changedTouches);
+    });
+});
