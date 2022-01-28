4dc12d55706a524268a2ad0bf0023567a44bd953
Quadath <Quadatz@users.noreply.github.com>
2022-01-28 18:59:29 +0200

    Promise

--- js/script.js
+++ js/script.js
@@ -1,16 +1,70 @@
-'use strict';
+'use strict'
 
-const persone = {
-    name: 'Alex',
-    tel: '+744444444',
-    parents: {
-        mom: 'Olga',
-        dad: 'Mike'
-    }
+console.log("Запрос данных..");
+
+const req = new Promise((resolve, reject) => {
+    setTimeout(() =>
+    {
+        console.log('Подготовка данных...');
+        const product = {
+            name: 'TV',
+            price: 2000,
+        };
+        resolve(product);
+    }, 2000);
+});
+
+req.then((product) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            product.staus = 'order';
+            // reject();
+            resolve(product);
+        }, 2000);
+    }).then(data => {
+        data.modify = true;
+        return data;
+    }).then((data) => {
+        console.log(data);
+    });
+}).catch(() => {
+    console.log('Ошибка.');
+}).finally(() => {
+    console.log('finally');
+});
+
+
+
+
+const test = time => {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(), time);
+    });
 };
 
-// console.log(JSON.parse(JSON.stringify(persone)))
-const clone = JSON.parse(JSON.stringify(persone));
-clone.parents.mom = 'Ann';
+test(1000).then(() => console.log('1000ms'));
+test(2000).then(() => console.log('2000ms'));
 
-console.log(clone);
+//Wait until all promised end
+Promise.all([test(1000), test(2000)]).then(() => {
+    console.log('all');
+});
+
+//Wait until one promise completed
+Promise.race([test(1000), test(2000)]).then(() => {
+    console.log('first');
+});
+//BAD
+// setTimeout(() =>
+// {
+//     console.log('Подготовка данных...');
+//     const product = {
+//         name: 'TV',
+//         price: 2000,
+//     };
+
+//     setTimeout(() => {
+//         product.staus = 'order';
+//         console.log(product);
+//     }, 2000);
+// }, 2000);
