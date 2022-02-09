1c2d641bcf3c2df8d24ade2bf83b9b640efea6ce
Quadath <Quadatz@users.noreply.github.com>
2022-02-09 17:44:55 +0200

    local storage

--- js/script.js
+++ js/script.js
@@ -1,51 +1,28 @@
 'use strict';
 
-// // filter
+// localStorage.setItem('name', "value");
 
-// const names = ['Ivan', 'Ann', 'Ksenia', 'Voldemart', 'Dmitro'];
+// let name = localStorage.getItem('name');
 
-// const shortNames = names.filter((name) => {
-//     return name.length < 5;
-// });
+// localStorage.removeItem('name');
 
-// console.log(shortNames);
+// localStorage.clear();
 
-//map
-
-// const answers = ['IvAn', 'AnnA', 'Hello'];
-
-// const result = answers.map(item => {return item.toLowerCase();});
-// console.log(result);
-
-// let answers = ['IvAn', 'AnnA', 'Hello'];
-
-// answers = answers.map(item => {return item.toLowerCase();});
-// console.log(answers);
-
-// every/some
-
-// const some = [3, 'qw', 'fsdffsdf'];
-
-// console.log(some.some(item => typeof(item) === 'number')); //true
-
-// console.log(some.every(item => typeof(item) === 'number')); //false
-
-// const arr = [4, 5, 1, 3, 2, 6];
-// const arr = ['apple', 'pear', 'plum'];
+const checkbox = document.querySelector('#checkbox'),
+    text = document.querySelector('#text');
 
 
-// const result = arr.reduce((sum, current) => sum + current);
-// const result = arr.reduce((sum, current) => sum + `, ${current}`);
-// console.log(result);
+if(localStorage.getItem('isChecked')) {
+    checkbox.checked = true;
+}
 
-const obj = {
-    ivan: 'persone',
-    ann: 'persone',
-    dog: 'animal',
-    cat: 'animal'
-};
+if(localStorage.getItem('text')) {
+    text.value = localStorage.getItem('text');
+}
 
-const newArr = Object.entries(obj)
-    .filter(item => item[1] == 'persone')
-    .map(item => item[0]);
-console.log(newArr);
+checkbox.addEventListener('change', () => {
+    localStorage.setItem('isChecked', true);
+});
+text.addEventListener('change', () => {
+    localStorage.setItem('text', text.value);
+});
