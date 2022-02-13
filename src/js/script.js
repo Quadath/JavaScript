824e95f1a1df7b791ff9cf05ca9f045c12ffd75e
Quadath <quadath@gmail.com>
2022-02-13 21:32:59 +0200

    module structure

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+import {one, two} from './main.js'; //переименование переменной
+
+// import * as data from './main'; //Импортировать всё
+import sayHi from './main.js';
+
+console.log(`one is ${one}`);
+
+// data.sayHi();
+sayHi();
