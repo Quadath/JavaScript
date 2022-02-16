0d6ca75c3b6e1634d5099c831465e3253b922896
Quadath <quadath@gmail.com>
2022-02-16 12:48:10 +0200

    fucn generator

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,31 @@
 'use strict';
 
-try {
-    console.log('Normal');
-    console.log(a);
-} catch(error) {
-    console.log(error.name);
-    console.log(error.stack);
+// function* generator() {
+//     yield 'B';
+//     yield 'r';
+//     yield 'u';
+//     yield 'h';
+// }
+
+// const str = generator();
+
+// console.log(str.next());
+// console.log(str.next());
+// console.log(str.next());
+// console.log(str.next().value);
+
+function* count(n) {
+    for (let i = 0; i < n; i++) {
+        yield i;
+    }
 }
 
-console.log('Still normal');
+for (let k of count(7)) {
+    console.log(k);
+}
+
+// const counter = count(7);
+
+// console.log(counter.next());
+// console.log(counter.next());
+// console.log(counter.next());
