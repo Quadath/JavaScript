39fb95ff952378cc02ba138967d19a6db767ad02
Quadath <quadath@gmail.com>
2022-02-17 06:57:42 +0200

    hi

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -1,2 +1,2 @@
-(()=>{"use strict";try{console.log("Normal"),console.log(a)}catch(o){console.log(o.name),console.log(o.stack)}console.log("Still normal")})();
+console.log("hi");
 //# sourceMappingURL=bundle.js.map
